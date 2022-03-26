0e9a2f9ea12ed7f7d251c797d97ea657a6b71d54
Uchkun <85468493+uchkun-ux@users.noreply.github.com>
2022-03-26 20:14:16 +0500

    Merge branch 'main' of github.com:uchkun-ux/CRM

--- sucrm/urls.py
+++ sucrm/urls.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+LoginView, 
+LogoutView, 
+PasswordResetView,
+PasswordResetDoneView,
+PasswordResetConfirmView,
+PasswordResetCompleteView
+)
 from django.urls import path, include
 from leads.views import HomeView, SigupView
 
@@ -11,5 +18,9 @@ urlpatterns = [
     path('agents/', include('agents.urls', namespace="agents")),
     path('signup/', SigupView.as_view(), name="signup"),
     path('login/', LoginView.as_view(), name="login"),
+    path('password-reset-view/', PasswordResetView.as_view(), name="password_reset_view"),
+    path('password-reset-done/', PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('password_reset_complete/', PasswordResetCompleteView.as_view(), name="password_complete_done"),
+    path('password_reset_confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     path('logout/', LogoutView.as_view(), name="logout")
 ]
