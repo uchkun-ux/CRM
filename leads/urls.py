5bf0c21e29bbafc8b2172eb0709694c794e2445a
Uchkun <85468493+uchkun-ux@users.noreply.github.com>
2022-03-29 17:58:18 +0500

    Story 182

--- leads/urls.py
+++ leads/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('<int:pk>/', LeadDetailView.as_view(), name='detallar'),
     path('<int:pk>/update_detail', LeadUpdateView.as_view(), name="update"),
     path('<int:pk>/delete', LeadDeleteView.as_view(), name="delete"),
+    path('<int:pk>/agent_ident', AgentAssignView.as_view(), name="agent_ident"),
     path('create-leads/', LeadCreateView.as_view(), name="lead-create")
-]
+] 
